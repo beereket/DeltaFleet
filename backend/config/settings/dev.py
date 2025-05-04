e66b17183807852e523df05a69d59b200a0cdd21
beereket <b_yergali@outlook.com>
2025-05-04 16:20:11 +0500

    setup backend

---
+++ backend/config/settings/dev.py
@@ -0,0 +1,32 @@
+from .base import *
+from decouple import config
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']  # during development
+
+# Use SQLite for simple local dev OR PostgreSQL
+# If you're using Docker from the beginning, keep this PostgreSQL config
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': config('POSTGRES_DB', default='deltafleet'),
+    'USER': config('POSTGRES_USER', default='deltauser'),
+    'PASSWORD': config('POSTGRES_PASSWORD', default='deltapass'),
+    'HOST': config('POSTGRES_HOST', default='localhost'),
+    'PORT': config('POSTGRES_PORT', default='5432'),
+}
+
+# Show detailed error logs in development
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
